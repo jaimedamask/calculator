e87384bc8de6910e31ddf41a5726bc5d12212c7f
jaimedamask <123277441+jaimedamask@users.noreply.github.com>
2024-03-06 17:45:21 -0500

    Feat: Add operate function and variables for number and operator inputs

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
+let num1;
+let num2;
+let operator;
+
 function add(a, b) {
     return a + b;
 };
@@ -13,3 +17,21 @@ function multiply(a, b) {
 function divide(a, b) {
     return a / b;
 };
+
+function operate(num1, num2, operator) {
+    if (operator === addition) {
+        return add(num1, num2);
+    };
+
+    if (operator === subtraction) {
+        return subtract(num1, num2);
+    };
+
+    if (operator === multiplication) {
+        return multiply(num1, num2);
+    };
+
+    if (operator === division) {
+        return divide(num1, num2);
+    };
+}
