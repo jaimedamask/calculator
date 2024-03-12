0e03825f708ef2b6c6fde415dda525db407cd17b
jaimedamask <123277441+jaimedamask@users.noreply.github.com>
2024-03-11 20:20:14 -0400

    Feat: Add event listeners and functions to buttons

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 let num1;
 let num2;
 let operator;
+const display = document.getElementById('display');
 
 function add(a, b) {
     return a + b;
@@ -19,19 +20,71 @@ function divide(a, b) {
 };
 
 function operate(num1, num2, operator) {
-    if (operator === addition) {
-        return add(num1, num2);
+    let n1 = parseFloat(num1);
+    let n2 = parseFloat(num2);
+
+    if (operator === 'addition') {
+        return add(n1, n2);
     };
 
-    if (operator === subtraction) {
-        return subtract(num1, num2);
+    if (operator === 'subtraction') {
+        return subtract(n1, n2);
     };
 
-    if (operator === multiplication) {
-        return multiply(num1, num2);
+    if (operator === 'multiplication') {
+        return multiply(n1, n2);
     };
 
-    if (operator === division) {
-        return divide(num1, num2);
+    if (operator === 'division') {
+        return divide(n1, n2);
     };
 }
+
+const calculator = document.querySelector('#calculator');
+const buttons = document.querySelector('#buttons');
+
+buttons.addEventListener('click', e => {
+    if (e.target.matches('button')) {
+        const button = e.target; 
+        const operation = button.dataset.action;
+        const buttonContent = button.textContent;
+        let displayedNum = display.textContent;
+
+        if (!operation) {
+            if (displayedNum === '0') {
+                display.textContent = buttonContent;
+            } else {
+                display.textContent = displayedNum += buttonContent;
+            }
+        }
+
+        if (
+            operation === 'addition' ||
+            operation === 'subtraction' ||
+            operation === 'multiplication' ||
+            operation === 'division'
+        ) {
+            calculator.dataset.num1 = displayedNum;
+            calculator.dataset.operator = operation;
+            
+            display.textContent = 0;
+        }
+
+        if (operation === 'decimal') {
+            display.textContent = displayedNum + '.';
+        }
+
+        if (operation === 'clear') {
+            display.textContent = 0;
+        }
+
+        if (operation === 'calculate') {
+            num1 = calculator.dataset.num1;
+            num2 = displayedNum;
+            
+            operator = calculator.dataset.operator;
+            let result = operate(num1, num2, operator);
+            display.textContent = result;
+        }
+    }
+})
